62a9da5b1291d6f60181dbdd9607d1805b3d0ca1
Klaus1710 <96344516+Klaus1710@users.noreply.github.com>
2023-05-13 09:35:52 +0530

    Added solution - LeetHub

--- Array Operations - GFG/array-operations.cpp
+++ Array Operations - GFG/array-operations.cpp
@@ -8,24 +8,35 @@ class Solution {
   public:
     int arrayOperations(int n, vector<int> &arr) {
         // code here
+        //flag variable to be turned on when a Nonzero element is encountered
         bool Nonzero=false;
+        //total no of subarrays
         int res=0;
+        //total no of zeroes
         int zC=0;
         for(int i=0; i<n; i++)
         {
             if(arr[i] == 0) 
             {
                  if(Nonzero) res++;
+                 //if a 0 is encounterd after encountering a Nonzero element increase no of subarrays
+                 
                  Nonzero = false;
-                 zC++;
+                 
+                 zC++;//count no of total no of zeroes in the array
             }
             else
             {
                 Nonzero = true;
+                //flag turned on encountering a nonzero element
             }
         }
-        if(zC==0) return -1;
+        if(zC==0) return -1;//if no of 0s is 0 return -1
+        
+        //if last subarray is left without encountering a 0 count it too
         if(Nonzero) res++;
+        
+        //return the result
         return res;
         
     }
